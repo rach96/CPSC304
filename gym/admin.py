9102c00797686ac51e9c562b17c4d5d5b7e7a27a
Rachel Sunderland <rachsunderland@gmail.com>
2017-03-21 11:29:32 -0700

    Added part of the sql file

--- gym/admin.py
+++ gym/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .sql_tables import Customer1
+#from .sql_tables import Customer1
 
 # Register your models here.
 
 
 #this is where we define users and their authorities
 
-admin.site.register(Customer1)
+#admin.site.register(Customer1)
 
--- gym/sql_tables.py
+++ gym/sql_tables.py
@@ -5,32 +5,32 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Customer1(models.Model ):
-    cusID = models.IntegerField(primary_key=True)
-    cusName = models.CharField( max_length= 30)
-    cusPhoneNumber = models.IntegerField(max_length=10)
-
-class Customer2(models.Model):
-    cusName = models.CharField(max_length=30, primary_key=True)
-    cusPhoneNumber = models.IntegerField(max_length=10, primary_key=True)
-    cusBirthday = models.DateField()
-    cusAddress  = models.CharField (max_length= 100)
-
-class Athlete(models.Model):
-    athleteTeam = models.CharField(max_length=20)
-    athleteID = models.ForeignKey(Customer1, on_delete=models.CASCADE, primary_key=True)
-
-class Member(models.Model):
-    memberID = models.ForeignKey(Customer1, on_delete=models.CASCADE, primary_key=True)
-    memberRenewalTime = models.DateField()
-    memberType = models.CharField(max_length=30) #eventually mabe a value from a list?
-
-class Equipment_checkIn_reserveOrcancel_return1(models.Model):
-    EquipType = models.CharField(max_length=20, primary_key=True)
-    EquipRate = models.FloatField
-    EquipDamageFee = models.FloatField()
-
-class Equipment_checkIn_reserveOrcancel_return2(models.Model):
-    EquipID = models.IntegerField(primary_key=True)
-    EquipType = models.CharField(max_length=20)
-    EquipCustID = models.ForeignKey(Customer1, on_delete=models.CASCADE)
+# class Customer1(models.Model ):
+#     cusID = models.IntegerField(primary_key=True)
+#     cusName = models.CharField( max_length= 30)
+#     cusPhoneNumber = models.IntegerField(max_length=10)
+#
+# class Customer2(models.Model):
+#     cusName = models.CharField(max_length=30, primary_key=True)
+#     cusPhoneNumber = models.IntegerField(max_length=10, primary_key=True)
+#     cusBirthday = models.DateField()
+#     cusAddress  = models.CharField (max_length= 100)
+#
+# class Athlete(models.Model):
+#     athleteTeam = models.CharField(max_length=20)
+#     athleteID = models.ForeignKey(Customer1, on_delete=models.CASCADE, primary_key=True)
+#
+# class Member(models.Model):
+#     memberID = models.ForeignKey(Customer1, on_delete=models.CASCADE, primary_key=True)
+#     memberRenewalTime = models.DateField()
+#     memberType = models.CharField(max_length=30) #eventually mabe a value from a list?
+#
+# class Equipment_checkIn_reserveOrcancel_return1(models.Model):
+#     EquipType = models.CharField(max_length=20, primary_key=True)
+#     EquipRate = models.FloatField
+#     EquipDamageFee = models.FloatField()
+#
+# class Equipment_checkIn_reserveOrcancel_return2(models.Model):
+#     EquipID = models.IntegerField(primary_key=True)
+#     EquipType = models.CharField(max_length=20)
+#     EquipCustID = models.ForeignKey(Customer1, on_delete=models.CASCADE)

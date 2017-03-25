dd77a35e029d42ae3ec8e4a916523195790e179e
Rachel Sunderland <rachsunderland@gmail.com>
2017-03-25 14:10:41 -0700

    Final Draft March 21st: got the sql script to kind of execute

--- gym/urls.py
+++ gym/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^viewC/$', views.viewC, name='viewC')
+    url(r'^viewC/$', views.viewC, name='viewC'),
+    url(r'^page1/$', views.page1, name='Page1'),
+    url(r'^page2/$', views.page2, name='Page2'),
+    url(r'^page3/$', views.page3, name='Page3'),
+    url(r'^page4/$', views.page4, name='Page4')
 ]
--- gym/views.py
+++ gym/views.py
@@ -3,16 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.db import connection
+from django.contrib.auth.decorators import login_required
+
 
 from gym.query import my_custom_sql
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the Awesome Gym.")
+
+def page1(request):
+    return HttpResponse("This is where the aggregations will go")
+
+def page2(request):
+    return HttpResponse("This is where the projections will go")
+
+#testPage
 def viewC(request):
     data = my_custom_sql(request)
     print(data)
     return render(request, 'gym/viewC.html', data)
 # this is where we link the html to our app. where we put REST
 
+@login_required
+def page3(request):
+    return HttpResponse("This is where we can delete tuples in clean room")
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Awesome Gym.")
+@login_required
+def page4(request):
+    return HttpResponse("This is where we can update items OR delete items so that there is a cascade")
+
 
